0efafc23f904f2a86c0aab68bd59aebc6028bd19
Guilherme Dantas <guikartman@gmail.com>
2021-01-09 21:20:21 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/resources/OrderResource.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/resources/OrderResource.java
@@ -1,12 +1,15 @@
 package com.devsuperior.dsdeliver.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
 import com.devsuperior.dsdeliver.services.OrderService;
@@ -22,5 +25,15 @@ public class OrderResource {
 	public ResponseEntity<List<OrderDTO>> findAll() {
 		List<OrderDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto) {
+		dto = service.insert(dto);
+		// Pega a Uri do objeto criado
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+					.buildAndExpand(dto.getId()).toUri();
+		// codigo 201
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
